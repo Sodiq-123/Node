4d1c36a65954147202151606c13157e252e99404
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-04-23 01:54:38 +0100

    Generate Express Code snippets

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-//require the mongoClient from mongodb module
-var MongoClient = require('mongodb').MongoClient;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//mongodb configs
-var connectionUrl = 'mongodb://localhost:27017/myproject',
-    sampleCollection = 'chapters';
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-//We need to insert these chapters into mongoDB
-var chapters = [{
-    'Title': 'Snow Crash',
-    'Author': 'Neal Stephenson'
-}, {
-    'Title': 'Snow Crash',
-    'Author': 'Neal Stephenson'
-}];
+var app = express();
 
-MongoClient.connect(connectionUrl, function (err, client) {
-    var db = client.db('myproject')
-    console.log("Connected correctly to server");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    // Get some collection
-    var collection = db.collection(sampleCollection);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    collection.insertMany(chapters, function (error, result) {
-        //here result will contain an array of records inserted
-        if (!error) {
-            console.log("Success :" + result.ops.length + " chapters inserted!");
-        } else {
-            console.log("Some error was encountered!");
-        }
-        client.close();
-    });
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// module.exports = app;
+app.listen(app.get('port'), function() {
+    console.log('Server up: http://localhost:' + app.get('port'));
 });
